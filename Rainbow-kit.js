ca28140c39ea3d8f5a1c51dbde8f6d9f1e9ce260
Sourin <98945276+heysourin@users.noreply.github.com>
2023-02-01 23:20:44 +0530

    Create Rainbow-kit.js

---
+++ Rainbow-kit.js
@@ -0,0 +1,47 @@
+//npm install @rainbow-me/rainbowkit
+
+//----------------------------------------App.js---------------------------------------//
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+const { chains, provider } = configureChains(
+  [polygonMumbai],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
+
+reportWebVitals();
+
+//----------------------------------------App.js---------------------------------------//
+import { ConnectButton } from "@rainbow-me/rainbowkit";
