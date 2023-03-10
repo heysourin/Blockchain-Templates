c2e0018e5b4d2d44c3b446a434afb2a20e489646
Sourin <98945276+heysourin@users.noreply.github.com>
2023-03-11 00:14:27 +0530

    Update deploy_and_verify.js

--- deploy_and_verify.js
+++ deploy_and_verify.js
@@ -7,9 +7,11 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`Contract deployed to ${simpleStorage.address}`);
+  
+  // !!! Notice the chainId here.
   if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
-    await simpleStorage.deployTransaction.wait(6);
-    await verify(simpleStorage.address, []);
+    await simpleStorage.deployTransaction.wait(3);
+    await verify(simpleStorage.address, []);// Don't forget to add the parameters inside '[]' present in the constructor.
   }
 }
 
