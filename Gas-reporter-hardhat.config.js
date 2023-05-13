37194593f703be11f3918df65fd95deb11ad0c3f
Sourin <98945276+heysourin@users.noreply.github.com>
2023-05-13 19:18:19 +0530

    Create Gas-reporter-hardhat.config.js

---
+++ Gas-reporter-hardhat.config.js
@@ -0,0 +1,13 @@
+// npm i hardhat-gas-reporter
+
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  gasReporter: {
+    enabled: true,
+    // outputFile: "gas-report.txt",
+    noColors: true,
+  },
+};
