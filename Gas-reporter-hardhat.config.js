fe27588b37e76de86f156e07c245b8ddd70be416
Sourin <98945276+heysourin@users.noreply.github.com>
2023-06-07 11:51:34 +0530

    Update Gas-reporter-hardhat.config.js
    Gas Reporter hardhat.config.js template

--- Gas-reporter-hardhat.config.js
+++ Gas-reporter-hardhat.config.js
@@ -1,16 +1,18 @@
-// npm i hardhat-gas-reporter
+//npm i hardhat-gas-reporter
 
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-gas-reporter");
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
   gasReporter: {
     enabled: true,
     outputFile: "gas-report.txt",
     noColors: true,
     currency: "USD",
-    coinmarketcap: COINMARKETCAP_API_key,// required for USD data fetching
-    token: "MATIC",// if you want gas cos in Matic
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY, // required for USD data fetching
+    token: "MATIC", // if you want gas cos in Matic
   },
 };
